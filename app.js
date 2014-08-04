1aea3cd14dda5706da0eeeedf55861f94b2dd483
Paul Yi <paul.yi326@gmail.com>
2014-08-04 14:51:34 -0700

    server and is now live. now testing routes

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ var app = express();
 // mongoose.connect('mongodb://localhost/photoYarn');
 mongoose.connect('mongodb://MongoLab-1:8fslrNoqQA8bTtE9toqkplr32HsoWQO1fohSpbc1KbA-@ds050077.mongolab.com:50077/MongoLab-1');
 
+app.get('/', function(req, res) {
+    res.send('herro this is the main page')
+});
 
-app.use('/', express.static(__dirname + '/public'));
+app.get('/yarns', function(req, res) {
+    res.send('this will be called as soon as the app loads to send all the yarns')
+});
+
+app.post('/yarns', function(req, res) {
+    res.send('this will be the route where you post new yarns');
+});
+
+app.post('photo', function(req, res) {
+    res.send('this is the route where you post new photos to a yarn');
+});
 
 module.exports = app;
 
