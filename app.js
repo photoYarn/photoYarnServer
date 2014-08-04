850a92c46ed9cd8634f2e5aa806465776d8dfc7e
Paul Yi <paul.yi326@gmail.com>
2014-08-04 14:41:45 -0700

    trying to connect to mongolab instance

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var api = require('./db/api.js');
 var app = express();
 // connect to Mongo when the app initializes
 // mongoose.connect('mongodb://localhost/photoYarn');
+mongoose.connect('mongodb://MongoLab-1:8fslrNoqQA8bTtE9toqkplr32HsoWQO1fohSpbc1KbA-@ds050077.mongolab.com:50077/MongoLab-1');
+
 
 app.use('/', express.static(__dirname + '/public'));
 
