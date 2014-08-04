06227cdd0b08c169d28dfacd7c2a1effb0ea4406
Paul Yi <paul.yi326@gmail.com>
2014-08-04 14:18:20 -0700

    trying to make my sever.js work

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var mongoose = require('mongoose');
-var api = require('./db/api.js');
+// var mongoose = require('mongoose');
+// var api = require('./db/api.js');
 
 var app = express();
 // connect to Mongo when the app initializes
-mongoose.connect('mongodb://localhost/photoYarn');
+// mongoose.connect('mongodb://localhost/photoYarn');
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 module.exports = app;
 
