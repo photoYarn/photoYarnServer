a8ded48b82f6f442acdc4e68fbe0350cd1e401bb
Paul Yi <paul.yi326@gmail.com>
2014-08-04 13:57:15 -0700

    using gregs server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var app = require('./app.js');
-var http = require('http').Server(app);
+// var app = require('./app.js');
+// var http = require('http').Server(app);
 
+// var port = process.env.port || 8080;
+
+// http.listen(port, function(){
+//     console.log('listening on 3000');
+// });
+
+var express = require('express');
+var app = express();
+
+//Specify a port
 var port = process.env.port || 8080;
 
-http.listen(port, function(){
-    console.log('listening on 3000');
-});
+//Serve up files in public folder
+app.use('/', express.static(__dirname + '/public'));
 
+//Start up the website
+app.listen(port);
+console.log('Listening on port: ', port);
