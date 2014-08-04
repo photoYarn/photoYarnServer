b3998e771ac754db9009615ff2de87c319b9f88b
Paul Yi <paul.yi326@gmail.com>
2014-08-04 14:11:51 -0700

    using my serverjs now

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-// var app = require('./app.js');
-// var http = require('http').Server(app);
+var app = require('./app.js');
+var http = require('http').Server(app);
 
-// var port = process.env.port || 8080;
-
-// http.listen(port, function(){
-//     console.log('listening on 3000');
-// });
-
-var express = require('express');
-var app = express();
-
-//Specify a port
 var port = process.env.port || 8080;
 
-//Serve up files in public folder
-app.use('/', express.static(__dirname + '/public'));
+http.listen(port, function(){
+    console.log('listening on ' + port);
+});
 
-//Start up the website
-app.listen(port);
-console.log('Listening on port: ', port);
