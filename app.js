233a42abd70a8673b4d60217c423a60290385085
Paul Yi <paul.yi326@gmail.com>
2014-08-04 19:05:32 -0700

    able to retrieve data from mongolab instance

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({
 // api.removeAllPhotos();
 
 // connect to Mongo when the app initializes
-// mongoose.connect('mongodb://localhost/photoYarn');
-mongoose.connect('mongodb://MongoLab-1:8fslrNoqQA8bTtE9toqkplr32HsoWQO1fohSpbc1KbA-@ds050077.mongolab.com:50077/MongoLab-1');
+mongoose.connect('mongodb://localhost/photoYarn');
+// mongoose.connect('mongodb://MongoLab-1:8fslrNoqQA8bTtE9toqkplr32HsoWQO1fohSpbc1KbA-@ds050077.mongolab.com:50077/MongoLab-1');
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/public/index.html');
