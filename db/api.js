09d50236cdec80a091d6b2abded563857ad15e32
Paul Yi <paul.yi326@gmail.com>
2014-08-05 20:57:09 -0700

    refactored app.js and api.js.

--- db/api.js
+++ db/api.js
@@ -2,57 +2,35 @@ var Yarn = require('./models/yarn.js');
 var User = require('./models/user.js');
 var Photo = require('./models/photo.js');
 
-exports.createYarn = function(params) {
-    exports.createPhoto({link: params.link})
-    console.log('params.link', params.link);
-    return new Yarn({
-        caption: params.caption,
-        creatorId: params.creatorId,
-        links: [params.link]
+exports.createYarn = function(req, callback) {
+ 
+    new Yarn({
+        caption: req.body.caption,
+        creatorId: req.body.creatorId,
+        links: [req.body.link]
+    }).save(function(err, yarn, numAffected) {
+        callback(err, yarn, numAffected);
     });
-
 };
 
-exports.addPhoto = function(params, res) {
-    
-    exports.createPhoto({ link: params.link});
-
-    Yarn.findOne({_id: params.yarnId}, function(err, yarn) {
-        yarn.links.push(params.link);
+exports.addPhoto = function(req, callback) {
+        
+    Yarn.findOne({_id: req.body.yarnId}, function(err, yarn) {
+        yarn.links.push(req.body.link);
         console.log('yarn', yarn);
         yarn.save(function(err, yarn, num) {
             // console.log('err', err);
             // console.log('how many photos', yarn.links.length);
             // console.log('num', num);
-            if (err) {
-                res.send(err);
-            } else {
-                res.send(200, 'Kia is kewl');
-            }
+            callback(err, yarn, num);
         });
-        // instantiate photo model instance
     });
 };
 
 
-exports.getAllYarns = function(res) {
-    return Yarn.find();
-};
-
-exports.createPhoto = function(params) {
-    new Photo({
-        url: params.link
-    })
-    .save(function(err, photo, num) {
-        // console.log('err', err);
-        // console.log('photo', photo);
-        // console.log('num', num);
-    });
-};
-
-exports.getAllPhotos = function() {
-    Photo.find().exec(function(err, photos) {
-        console.log(photos);
+exports.getAllYarns = function(callback) {
+    return Yarn.find({}, function(err, yarns) {
+        callback(err, yarns);
     });
 };
 
