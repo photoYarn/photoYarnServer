782f321adb1a2d0f940c84feeaeea8a62646535b
Justin Cheung <jygsaw@gmail.com>
2014-08-20 12:46:16 -0700

    [TEST] Rename 'thread' to 'yarn' in feedTest for consistency

--- test/feedTest.js
+++ test/feedTest.js
@@ -3,40 +3,40 @@
 var request = require('supertest');
 var app = require('../app.js');
 var expect = require('chai').expect;
-var testUtils = require('./testUtils.js');
+var testUtils = require('./utils/testUtils.js');
 
-xdescribe('Feed API', function() {
-    var threadData;
-    // before(function(done) {
-    //     // populate database with target threads
-    //     threadData = testUtils.populateThreads({
-    //         numThreads: 2,
-    //         caption: 'Test Feed',
-    //         creatorId: '9600000',
-    //         link: 'http://www.bogus.com/99600000',
-    //     }, done);
-    // });
+describe('Feed API', function() {
+    var yarnData;
+    before(function(done) {
+        // populate database with target yarns
+        yarnData = testUtils.populateYarns({
+            numYarns: 2,
+            caption: 'Test Feed',
+            creatorId: '9600000',
+            link: 'http://www.bogus.com/99600000',
+        }, done);
+    });
 
-    xit('should retrieve all threads', function(done) {
-        // request all threads
+    xit('should retrieve all yarns', function(done) {
+        // request all yarns
         request(app)
             .get('/getAllYarns')
             .expect(200)
             .end(function(err, res) {
-                // build hash of expected threads
+                // build hash of expected yarns
                 var expected = {};
-                for (var i = 0; i < threadData.length; i++) {
-                    expected[threadData[i].caption] = false;
+                for (var i = 0; i < yarnData.length; i++) {
+                    expected[yarnData[i].caption] = false;
                 }
 
-                // mark returned threads
+                // mark returned yarns
                 var data = res.body;
                 for (var i = 0; i < data.length; i++) {
                     // TODO verify individual values
                     expected[data[i].caption] = true;
                 }
 
-                // verify all expected threads found
+                // verify all expected yarns found
                 for (var key in expected) {
                     expect(expected[key]).to.equal(true);
                 }
@@ -45,12 +45,12 @@ xdescribe('Feed API', function() {
             });
     });
 
-    xit('should retrieve the first X threads', function() {
+    xit('should retrieve the first X yarns', function() {
     });
 
-    xit('should retrieve threads from index X to Y', function() {
+    xit('should retrieve yarns from index X to Y', function() {
     });
 
-    xit('should retrieve the first X photos from each retrieved thread', function() {
+    xit('should retrieve the first X photos from each retrieved yarn', function() {
     });
 });
